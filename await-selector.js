54532d6cf20358a07597496e6f68ba0a9636facb
Rommel Santor <rommel@rommelsantor.com>
2017-06-12 08:04:40 -0700

    Create await-selector.js

--- await-selector.js
+++ await-selector.js
@@ -1,49 +1,56 @@
+/* global MutationObserver WebKitMutationObserver */
+
 const awaitSelector = (selector, rootNode, fallbackDelay) => new Promise((resolve, reject) => {
   try {
     const root = rootNode || document
-    const observerClass = MutationObserver || WebKitMutationObserver || undefined
-    const mutationObserverSupported = typeof observerClass === 'function'
-    
+    const ObserverClass = MutationObserver || WebKitMutationObserver || null
+    const mutationObserverSupported = typeof ObserverClass === 'function'
+
     let observer
-    
+
     const stopWatching = () => {
       if (observer) {
-        mutationObserverSupported ? observer.disconnect() : clearInterval(observer)
+        if (mutationObserverSupported) {
+          observer.disconnect()
+        } else {
+          clearInterval(observer)
+        }
+
         observer = null
       }
     }
 
     const findAndResolveElements = () => {
       const allElements = root.querySelectorAll(selector)
-      
+
       if (allElements.length === 0) return
-      
+
       let newElements = 0
-      
+
       if (!mutationObserverSupported) {
         const attributeForBypassing = 'data-mutationobserved'
 
-        allElements.forEach(el => {
+        allElements.forEach((el, i) => {
           if (typeof el[attributeForBypassing] === 'undefined') {
-            el[attributeForBypassing] = ''
-            ++newElements
+            allElements[i][attributeForBypassing] = ''
+            newElements += 1
           }
         })
       }
-      
+
       if (mutationObserverSupported || newElements > 0) {
         stopWatching()
         resolve(allElements)
       }
     }
-    
+
     if (mutationObserverSupported) {
-      observer = new observerClass(mutationRecords => {
+      observer = new ObserverClass(mutationRecords => {
         const nodesWereAdded = mutationRecords.reduce(
           (found, record) => found || (record.addedNodes && record.addedNodes.length > 0),
           false
         )
-        
+
         if (nodesWereAdded) {
           findAndResolveElements()
         }
@@ -56,8 +63,8 @@ const awaitSelector = (selector, rootNode, fallbackDelay) => new Promise((resolv
     } else {
       observer = setInterval(findAndResolveElements, fallbackDelay || 250)
     }
-    
-    window.addEventListener('load', findAndResolveElements)
+
+    (global || window).addEventListener('load', findAndResolveElements)
   } catch (exception) {
     reject(exception)
   }
